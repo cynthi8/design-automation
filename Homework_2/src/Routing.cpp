9c1c54696c06361a55a8a936b9c22b42a9b12ef1
Erin Cold <coldegret@protonmail.com>
2021-04-02 16:55:14 -0400

    Add minor changes before quitting with Live Share

--- Homework_2/src/Routing.cpp
+++ Homework_2/src/Routing.cpp
@@ -4,7 +4,6 @@
 #include "Graph.hpp"
 #include "Routing.hpp"
 
-
 void Routing::Route(Graph graph, Placement place)
 {
 	//This function needs to populate the rows such that we can do channel routing
@@ -16,10 +15,10 @@ void Routing::Route(Graph graph, Placement place)
 	Row rowTop;
 	Row rowBottom;
 	vector<pair<int, int>> Terminals;
-	this->Rows.resize(place.m_grid.size());
+	this->Rows.resize(place.m_grid.m_rows);
 
 	// go through the entire 2D grid that's been placed
-	for (i = 0; i < place.m_grid.size(); i++)
+	for (i = 0; i < place.m_grid.m_rows; i++)
 	{
 		for (j = 0; i < place.m_grid[i].size(); j++)
 		{
@@ -34,7 +33,8 @@ void Routing::Route(Graph graph, Placement place)
 			for (auto k : Terminals) //add terminals to row in order
 			{
 				Terminal term(k.second, cell_id);
-				switch (k.first) {
+				switch (k.first)
+				{
 				case 1:
 				case 2:
 					rowTop.m_Top.push_back(term);
@@ -56,19 +56,20 @@ void Routing::Route(Graph graph, Placement place)
 			maxSize = Rows[i].m_Top.size();
 	}
 
-	//pad the end of the rows with zeros
+	// Pad the end of the rows with zeros
 	Terminal empty(0, 0);
-	for (i = 0; i < this->Rows.size(); i++) {
+	for (i = 0; i < this->Rows.size(); i++)
+	{
 		for (j = 0; j < this->Rows[i].m_Top.size() - maxSize; j++)
 			this->Rows[i].m_Top.push_back(empty);
 		for (j = 0; j < this->Rows[i].m_Bot.size() - maxSize; j++)
 			this->Rows[i].m_Bot.push_back(empty);
 	}
 
-	//now I need to go and make the VCG graph
+	// Now I need to go and make the VCG graph
+	VGraph vgraph;
 
-	//Then just actually route the thing
-
+	// Then just actually route the thing
 
 	return;
 }
