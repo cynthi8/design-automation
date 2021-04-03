4e2b3ae35f3009093eb96ec169f002ca46d3d899
Erin Cold <coldegret@protonmail.com>
2021-04-03 00:53:07 -0400

    Cleanup main to specifically test memory error

--- Homework_2/src/Main.cpp
+++ Homework_2/src/Main.cpp
@@ -19,29 +19,13 @@ int main(int argc, char *argv[])
 {
     cout << "Hello World\n";
 
-    /*
-    if(argc != 3)
-    {
-        cout << "Error: wrong number of inputs.\n" << "What is this called? InputFile OutputFile" << endl;
-        exit(1);
-    }
-    */
-    vector<string> vargv(argv, argv + argc);
-
     Graph graph("Benchmarks/b_50_50");
-
+    Placement placement(graph, 10);
+    /*
     Placement placement(graph, 10);
     placement.ForceDirectedPlace();
     placement.ForceDirectedFlip();
     //Grid grid = placement.Export();
-
-
-    //int test = tests(argc, argv);
-    /*
-    Graph graph(vargv[1]);
-    Placement place(graph);
-    Routing Route(graph, place);
-    WriteToMagic(routing, vargv[2]);
     */
 
     return 0;
